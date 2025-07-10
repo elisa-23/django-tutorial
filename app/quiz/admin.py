f9133d217a2eddc9bd19ae79fe2d3709903f264a
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-07-10 10:49:09 -0400

    serializer

--- app/quiz/admin.py
+++ app/quiz/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import Users
+from .models import Quizzes
+from .models import Questions
 
-admin.site.register(BlogPost)
+admin.site.register(Users)
+admin.site.register(Quizzes)
+admin.site.register(Questions)
