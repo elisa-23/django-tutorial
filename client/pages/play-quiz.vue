e350fd312c91e8f7e52d978c1bb9d40ba5ffb14c
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-07-28 11:16:51 -0400

    Update play-quiz.vue

--- client/pages/play-quiz.vue
+++ client/pages/play-quiz.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
     <h1>Play Quiz</h1>
-    <div v-if="type === 'mc'"></div>
-    <div v-if="type === 'tf'"></div>
-    <div v-if="type === 'n'"></div>
-    <div v-if="type === 'dd'"></div>
   </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
-  type: "mc" | "tf" | "n" | "dd";
+  questionType: "mc" | "tf" | "n" | "dd";
 }>();
 
 /* async function fetchQuiz(quizId: number) {
