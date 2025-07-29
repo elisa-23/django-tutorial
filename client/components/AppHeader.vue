d840ce5c0db0610a675132cab4a418c5899ee735
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-07-29 08:38:24 -0400

    finsihing up log out system

--- client/components/AppHeader.vue
+++ client/components/AppHeader.vue
@@ -6,12 +6,16 @@
             <NuxtLink to="/SignIn">
                 <button v-if="!signin" class="bg-white rounded-md w-[100%] h-[50%] text-center hover:bg-[#E6E6FA] active:bg-[#CCCCFF] m-auto">Sign In</button>
             </NuxtLink>
+            <button v-if="signin" class="bg-white rounded-md w-[100%] h-[50%] text-center hover:bg-[#E6E6FA] active:bg-[#CCCCFF] m-auto" @click="userStore.signOut">
+                Sign Out
+            </button>
         </div>
     </div>
 </template>
 
 <script setup>
-const username = 'example'
-let signin = false;
+const userStore = useUserStore();
+const signin = computed(() => userStore.isSignedIn);
+const username = computed(() => userStore.userInfo.username);
 
 </script>
